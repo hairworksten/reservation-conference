7c2999a59b471812d7f453227c832388b3b55782
hairworksten <keiolinedaibakuha@gmail.com>
2025-07-05 17:34:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ async function checkReservation() {
     loadingMessage.style.display = 'block';
     
     try {
-        const response = await fetch(`${API_BASE_URL}/api/reservation-conference`, {
+        const response = await fetch(`${API_BASE_URL}/api/check-reservation`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
